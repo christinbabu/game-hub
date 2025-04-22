4cfe9ec5b8b1dfca9328ef67d0b254ae9cf484b3
christin <johnchristinchriz@gmail.com>
2025-04-22 20:32:58 +0530

    Fixed indention

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,14 +1,21 @@
 import { Badge } from "@chakra-ui/react";
 
 interface Props {
-    score: number;
+	score: number;
 }
 
-const CriticScore = ({score} : Props) => {
-    let color = score> 74 ? 'green' : score > 60 ? 'yellow' : '';
-  return (
-    <Badge  fontSize='10px' paddingX={2} borderRadius='4px' colorScheme={color}>{score}</Badge>
-  )
-}
+const CriticScore = ({ score }: Props) => {
+	let color = score > 74 ? "green" : score > 60 ? "yellow" : "";
+	return (
+		<Badge
+			fontSize="10px"
+			paddingX={2}
+			borderRadius="4px"
+			colorScheme={color}
+		>
+			{score}
+		</Badge>
+	);
+};
 
-export default CriticScore
+export default CriticScore;
